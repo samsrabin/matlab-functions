564c7fcd1cfb220caf80e8698f795eb534b44865
Sam Rabin <sam.rabin@kit.edu>
2020-08-19 13:40:16 +0200

    lpjgu_matlab_table2array.m actually works with 1d-to-3d as well.

--- lpjgu_matlab_table2array.m
+++ lpjgu_matlab_table2array.m
@@ -3,8 +3,8 @@ function out_yxc = lpjgu_matlab_table2array(in_array, target_size)
 Ndims_in = length(find(size(in_array)>1)) ;
 Ndims_out = length(target_size) ;
 
-if ~((Ndims_in==1 && Ndims_out==2) || (Ndims_in==2 && Ndims_out==3))
-    error('This only works with 1d-to-2d or 2d-to-3d')
+if ~((Ndims_in==1 && any(Ndims_out==[2 3])) || (Ndims_in==2 && Ndims_out==3))
+    error('This only works with 1d-to-(2d or 3d) or 2d-to-3d')
 elseif numel(in_array) ~= prod(target_size)
     error('Mismatch in number of elements from in to out')
 end
