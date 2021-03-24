ecc673f4a174c9949b7b36352e6668770a2debb7
Sam Rabin <sam.rabin@kit.edu>
2021-03-24 16:13:01 +0100

    get_system_name updated to work for Unicluster2.

--- get_system_name.m
+++ get_system_name.m
@@ -11,6 +11,17 @@ switch homedir
         thisSystem = 'ssr_mac' ;
     case {'/home/rabin-s'}
         thisSystem = 'ssr_keal' ;
+    case {'/home/kit/imk-ifu/lr8247'}
+        thisCluster = getenv('CLUSTER') ;
+        switch thisCluster
+            case {'uc2'}
+                thisSystem = 'ssr_uc2' ;
+            case ''
+                error('CLUSTER variable not set')
+            otherwise
+                error('thisSystem not recognized; home folder %s, CLUSTER %s not recognized', ...
+                    homedir, thisCluster)
+        end
     otherwise
         error('thisSystem not recognized; home folder %s', homedir)
 end
