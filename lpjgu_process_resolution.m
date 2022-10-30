c0dc7807e242163c39763f8ef4c4fc2a97a1eb9f
Sam Rabin <sam.rabin@gmail.com>
2022-10-30 16:31:16 -0600

    Bugfix for lpjgu_process_resolution() when X and Y resolutions differ.

--- lpjgu_process_resolution.m
+++ lpjgu_process_resolution.m
@@ -17,13 +17,13 @@ else
     if verboseIfNoMat || verbose
         disp('      Determining X and Y resolution...')
     end
-    if ~(xres_in>0)
-        unique_lats = unique(in_lats) ;
-        xres_out = min(abs(unique_lats(1:end-1)-unique_lats(2:end))) ;
-    end
     if ~(yres_in>0)
+        unique_lats = unique(in_lats) ;
+        yres_out = min(abs(unique_lats(1:end-1)-unique_lats(2:end))) ;
+    end
+    if ~(xres_in>0)
         unique_lons = unique(in_lons) ;
-        yres_out = min(abs(unique_lons(1:end-1)-unique_lons(2:end))) ;
+        xres_out = min(abs(unique_lons(1:end-1)-unique_lons(2:end))) ;
     end
     if verboseIfNoMat || verbose
         disp(['      Assuming X res. = ' num2str(xres_out) ', Y res. = ' num2str(yres_out)])
