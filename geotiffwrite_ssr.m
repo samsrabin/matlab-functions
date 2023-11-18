b288812528197215c8901ddc099107dcf291c00f
Sam Rabin <sam.rabin@gmail.com>
2023-11-18 12:33:04 -0600

    geotiffwrite_ssr(): Add verbose varargin (default true).

--- geotiffwrite_ssr.m
+++ geotiffwrite_ssr.m
@@ -1,8 +1,16 @@
-function geotiffwrite_ssr(filename, A, R, ndval)
+function geotiffwrite_ssr(filename, A, R, ndval, varargin)
 % GEOTIFFWRITE_SSR
 % As GEOTIFFWRITE, but takes the extra step (using GDAL Utilities) of
 % adding a NoData value where there are NaNs in A. Also automatically does
 % flipud before writing.
+
+verbose = true ;
+if ~isempty(varargin)
+    verbose = varargin{1} ;
+    if length(varargin) > 1
+        error('geotiffwrite_ssr() takes at most 1 optional argument: verbose')
+    end
+end
 
 if ~strcmp(filename(end-3:end),'.tif')
     filename = [filename '.tif'] ;
@@ -16,10 +24,14 @@ end
 A2 = flipud(A) ;
 A2(isnan(A2)) = ndval ;
 
-disp('Saving GeoTIFF...')
+if verbose
+    disp('Saving GeoTIFF...')
+end
 geotiffwrite(filename_tmp,A2,R) ;
 
-disp('Embedding nodata value...')
+if verbose
+    disp('Embedding nodata value...')
+end
 % thisFmt = ['source ~/.bash_profile; gdal_translate -a_nodata %s "%s" "%s"'] ;
 thisFmt = '/Library/Frameworks/GDAL.framework/Programs/gdal_translate -a_nodata %s "%s" "%s"' ;
 [s,w] = unix(sprintf(thisFmt, num2str(ndval), filename_tmp, filename)) ;
