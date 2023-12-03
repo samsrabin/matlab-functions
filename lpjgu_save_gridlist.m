da399fc0680ac9cc4ac3a4f2966974c0c2aa698a
Sam Rabin <sam.rabin@gmail.com>
2023-12-03 13:52:11 -0700

    Add do_randomize option to lpjgu_save_gridlist().

--- lpjgu_save_gridlist.m
+++ lpjgu_save_gridlist.m
@@ -6,6 +6,7 @@ p = inputParser ;
 addRequired(p, 'lonlats', is_ok_lonlats) ;
 addRequired(p, 'out_file', @ischar) ;
 addOptional(p, 'out_formatSpec_gridlist', '%4.2f %4.2f\n', @ischar)
+addOptional(p, 'do_randomize', true, @islogical)
 parse(p, lonlats, out_file, varargin{:});
 
 % Extract input arguments from p.Results
@@ -22,10 +23,12 @@ end ; clear f
 clear p pFields
 
 % Randomize order
-rng(19870724) ;
-Ncells = size(lonlats, 1) ;
-rdmsam  = randsample(Ncells, Ncells) ;
-lonlats = lonlats(rdmsam,:) ;
+if do_randomize
+    rng(19870724) ;
+    Ncells = size(lonlats, 1) ;
+    rdmsam  = randsample(Ncells, Ncells) ;
+    lonlats = lonlats(rdmsam,:) ;
+end
 
 % Save to file
 fid = fopen(strrep(out_file, '\ ', ' '), 'w') ;
