2c411e186b79d087ae0af6593f06c99592129166
Sam Rabin <sam.rabin@gmail.com>
2023-12-03 13:49:49 -0700

    Add lpjgu_save_gridlist().

---
+++ lpjgu_save_gridlist.m
@@ -0,0 +1,36 @@
+function lpjgu_save_gridlist(lonlats, out_file, varargin)
+
+% Set up & parse input arguments
+is_ok_lonlats = @(x) isnumeric(lonlats) && ismatrix(lonlats) && size(lonlats, 2)==2 ;
+p = inputParser ;
+addRequired(p, 'lonlats', is_ok_lonlats) ;
+addRequired(p, 'out_file', @ischar) ;
+addOptional(p, 'out_formatSpec_gridlist', '%4.2f %4.2f\n', @ischar)
+parse(p, lonlats, out_file, varargin{:});
+
+% Extract input arguments from p.Results
+pFields = fieldnames(p.Results) ;
+Nfields = length(pFields) ;
+for f = 1:Nfields
+    thisField = pFields{f} ;
+    if ~exist(thisField,'var')
+        eval(['global ' thisField ';']) ;
+        eval([thisField ' = p.Results.' thisField ' ;']) ;
+    end
+    clear thisField
+end ; clear f
+clear p pFields
+
+% Randomize order
+rng(19870724) ;
+Ncells = size(lonlats, 1) ;
+rdmsam  = randsample(Ncells, Ncells) ;
+lonlats = lonlats(rdmsam,:) ;
+
+% Save to file
+fid = fopen(strrep(out_file, '\ ', ' '), 'w') ;
+fprintf(fid, out_formatSpec_gridlist, lonlats') ;
+fprintf(fid, '%s', '') ;  % Blank line
+fclose(fid) ;
+
+end
